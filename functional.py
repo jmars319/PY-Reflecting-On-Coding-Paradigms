4e72b0a053fab2e58763d9db6c95897f0acc7315
Jason <jam319@protonmail.com>
2022-08-24 16:39:31 -0400

    completed paradigms, have not added questions yet

---
+++ functional.py
@@ -0,0 +1,8 @@
+# Implement a function that will flatten and sort an array of integers in ascending order, and which adheres to a functional programming paradigm.
+
+def flatten_and_sort(array):
+    arr = []
+    for item in array:
+        for i in item:
+            arr.append(i)
+    return sorted(arr)
